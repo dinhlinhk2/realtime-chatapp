b9442fbb76eb00632473b0756539f2f306eae5fb
nguyendinhlinh65@gmail.com <nguyendinhlinh65@gmail.com>
2025-03-16 12:05:07 +0700

    project comlated2

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ app.use(express.json({ limit: '50mb' }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
   })
